dfdefa3dcc29fc6a28e3f08bbd991e08ea3827a2
Erlend Røsjø <erlend@erlend.net>
2018-01-20 14:57:04 +0100

    Added app skeletons

---
+++ exercise-1/exercise-1.test.js
@@ -0,0 +1,36 @@
+const exercise1 = require("./exercise-1");
+
+describe("fizzBuzz", () => {
+  test("please name me", () => {
+    // some test calling exercise1.fizzBuzz(someinput)
+  });
+});
+
+describe("fizzBuzzRange", () => {
+  test("please name me as well", () => {
+    const correctResult = [
+      1,
+      2,
+      "Fizz",
+      4,
+      "Buzz",
+      "Fizz",
+      7,
+      8,
+      "Fizz",
+      "Buzz",
+      11,
+      "Fizz",
+      13,
+      14,
+      "FizzBuzz",
+      16,
+      17,
+      "Fizz",
+      19,
+      "Buzz"
+    ];
+
+    // Some test calling exercise1.fizzBuzzRange(someinput)
+  });
+});
---
+++ exercise-2/exercise-2.js
@@ -0,0 +1,10 @@
+function fizzBuzz(input) {
+  return input;
+}
+
+function fizzBuzzRange(max) {
+  return [];
+}
+
+module.exports.fizzBuzz = fizzBuzz;
+module.exports.fizzBuzzRange = fizzBuzzRange;
